aaf6efe639008a21a2a62d75d4a99bb238e38328
Karlito <karlo02@live.fr>
2026-01-06 02:14:36 +0100

    add project

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "XXX",
+  authDomain: "XXX.firebaseapp.com",
+  projectId: "XXX",
+  storageBucket: "XXX.appspot.com",
+  messagingSenderId: "XXX",
+  appId: "XXX",
+};
